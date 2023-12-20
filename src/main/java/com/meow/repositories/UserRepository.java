d2d424eefcdaf0623742ccdb7b021708c84b7f8d
bachlq1901 <bachlq@metawaytech.com>
2023-12-20 08:44:30 +0700

    update

--- src/main/java/com/meow/repositories/UserRepository.java
+++ src/main/java/com/meow/repositories/UserRepository.java
@@ -2,11 +2,18 @@ package com.meow.repositories;
 
 import com.meow.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmailAndPhone(String email, String phone);
+
+    @Query(nativeQuery = true, value = "select * from user where (:email is null or email = :email) and (:phone is null or phone = :phone)")
+    Optional<User> findByEmailAndPhone(
+            @Param("email") String email,
+            @Param("phone") String phone
+    );
 }
